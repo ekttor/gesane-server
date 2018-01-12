c3fd941783167970428705229d8cb038f116342f
a053603420b <a053603420b@S09-PC02>
2018-01-12 10:01:08 +0100

    Commit Servidor

---
+++ src/main/java/eu/rafaelaznar/dao/specificimplementation/UsuarioAlumnoSpecificDaoImplementation.java
@@ -0,0 +1,305 @@
+/*
+ * Copyright (c) 2017-2018
+ *
+ * by Rafael Angel Aznar Aparici (rafaaznar at gmail dot com) & DAW students
+ *
+ * GESANE: Free Open Source Health Management System
+ *
+ * Sources at:
+ *                            https://github.com/rafaelaznar/gesane-server
+ *                            https://github.com/rafaelaznar/gesane-client
+ *                            https://github.com/rafaelaznar/gesane-database
+ *
+ * GESANE is distributed under the MIT License (MIT)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package eu.rafaelaznar.dao.specificimplementation;
+
+import eu.rafaelaznar.bean.genericimplementation.TableGenericBeanImplementation;
+import eu.rafaelaznar.bean.genericimplementation.ViewGenericBeanImplementation;
+import eu.rafaelaznar.bean.helper.FilterBeanHelper;
+import eu.rafaelaznar.bean.helper.MetaBeanHelper;
+import eu.rafaelaznar.bean.meta.helper.MetaObjectGenericBeanHelper;
+import eu.rafaelaznar.bean.meta.helper.MetaPropertyGenericBeanHelper;
+import eu.rafaelaznar.bean.publicinterface.GenericBeanInterface;
+import eu.rafaelaznar.bean.specificimplementation.UsuarioSpecificBeanImplementation;
+import eu.rafaelaznar.dao.genericimplementation.TableGenericDaoImplementation;
+import eu.rafaelaznar.factory.BeanFactory;
+import eu.rafaelaznar.helper.Log4jHelper;
+import eu.rafaelaznar.helper.SqlHelper;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+
+public class UsuarioAlumnoSpecificDaoImplementation extends TableGenericDaoImplementation {
+
+    private Integer idCentrosanitario = 0;
+    private Integer idUsuario = 0;
+
+    public UsuarioAlumnoSpecificDaoImplementation(Connection oPooledConnection, MetaBeanHelper oPuserBean_security, String strWhere) throws Exception {
+        super("usuario", oPooledConnection, oPuserBean_security, strWhere);
+
+        UsuarioSpecificBeanImplementation oUsuario = (UsuarioSpecificBeanImplementation) oPuserBean_security.getBean();
+        idUsuario = oUsuario.getId();
+        idCentrosanitario = oUsuario.getId_centrosanitario();
+
+        //MetaBeanHelper oMetaBeanHelper = oUsuario.getObj_tipousuario();
+        //CentrosanitarioSpecificBeanImplementation oCentrosanitario = (CentrosanitarioSpecificBeanImplementation) oMetaBeanHelper.getBean();
+        strSQL = "SELECT * FROM usuario u WHERE u.id_centrosanitario = " + idCentrosanitario;
+    }
+
+    @Override
+    public Integer set(TableGenericBeanImplementation oBean) throws Exception {
+        PreparedStatement oPreparedStatement = null;
+        ResultSet oResultSet = null;
+        Integer iResult = 0;
+        Boolean insert = true;
+        try { // Retocar metodo set-update para insetar solo Nombre, Apellidos y email
+                strSQL = "UPDATE " + ob;
+                strSQL += " SET ";
+                strSQL += oBean.toPairs();
+                strSQL += " WHERE id=? ";
+                oPreparedStatement = oConnection.prepareStatement(strSQL, Statement.RETURN_GENERATED_KEYS);
+                oPreparedStatement.setInt(1, oBean.getId());
+                iResult = oPreparedStatement.executeUpdate();
+            
+            if (iResult < 1) {
+                String msg = this.getClass().getName() + ": set";
+                Log4jHelper.errorLog(msg);
+                throw new Exception(msg);
+            }
+            if (insert) {
+                oResultSet = oPreparedStatement.getGeneratedKeys();
+                oResultSet.next();
+                iResult = oResultSet.getInt(1);
+            }
+        } catch (Exception ex) {
+            String msg = this.getClass().getName() + ":" + (ex.getStackTrace()[0]).getMethodName() + " ob:" + ob;
+            Log4jHelper.errorLog(msg, ex);
+            throw new Exception(msg, ex);
+        } finally {
+            if (insert) {
+                if (oResultSet != null) {
+                    oResultSet.close();
+                }
+            }
+            if (oPreparedStatement != null) {
+                oPreparedStatement.close();
+            }
+        }
+        return iResult;
+    }
+    
+    @Override
+    public Long getCount(ArrayList<FilterBeanHelper> alFilter) throws Exception {
+        strSQL = "SELECT COUNT(*) FROM usuario u WHERE u.id_centrosanitario = " + idCentrosanitario;
+        PreparedStatement oPreparedStatement = null;
+        ResultSet oResultSet = null;
+        strSQL += SqlHelper.buildSqlFilter(alFilter);
+        Long iResult = 0L;
+        try {
+            oPreparedStatement = oConnection.prepareStatement(strSQL);
+            oResultSet = oPreparedStatement.executeQuery();
+            if (oResultSet.next()) {
+                iResult = oResultSet.getLong("COUNT(*)");
+            } else {
+                String msg = this.getClass().getName() + ": getcount";
+                Log4jHelper.errorLog(msg);
+                throw new Exception(msg);
+            }
+        } catch (Exception ex) {
+            String msg = this.getClass().getName() + ":" + (ex.getStackTrace()[0]).getMethodName() + " ob:" + ob;
+            Log4jHelper.errorLog(msg, ex);
+            throw new Exception(msg, ex);
+        } finally {
+            if (oResultSet != null) {
+                oResultSet.close();
+            }
+            if (oPreparedStatement != null) {
+                oPreparedStatement.close();
+            }
+        }
+        return iResult;
+    }
+
+    @Override
+    public MetaBeanHelper get(int id, int intExpand) throws Exception {
+        PreparedStatement oPreparedStatement = null;
+        ResultSet oResultSet = null;
+        strSQL += " AND u.id=? ";
+        TableGenericBeanImplementation oBean = null;
+        MetaBeanHelper oMetaBeanHelper = null;
+        try {
+            oPreparedStatement = oConnection.prepareStatement(strSQL);
+            oPreparedStatement.setInt(1, id);
+            oResultSet = oPreparedStatement.executeQuery();
+            oBean = (TableGenericBeanImplementation) BeanFactory.getBean(ob, oPuserSecurity);
+            if (oResultSet.next()) {
+                oBean = (TableGenericBeanImplementation) oBean.fill(oResultSet, oConnection, oPuserSecurity, intExpand);
+            } else {
+                oBean.setId(0);
+            }
+            ArrayList<MetaPropertyGenericBeanHelper> alMetaProperties = this.getPropertiesMetaData();
+            MetaObjectGenericBeanHelper oMetaObject = this.getObjectMetaData();
+            oMetaBeanHelper = new MetaBeanHelper(oMetaObject, alMetaProperties, oBean);
+        } catch (Exception ex) {
+            String msg = this.getClass().getName() + ":" + (ex.getStackTrace()[0]).getMethodName() + " ob:" + ob;
+            Log4jHelper.errorLog(msg, ex);
+            throw new Exception(msg, ex);
+        } finally {
+            if (oResultSet != null) {
+                oResultSet.close();
+            }
+            if (oPreparedStatement != null) {
+                oPreparedStatement.close();
+            }
+
+        }
+        return oMetaBeanHelper;
+    }
+
+    @Override
+    public MetaBeanHelper getPage(int intRegsPerPag, int intPage, LinkedHashMap<String, String> hmOrder, ArrayList<FilterBeanHelper> alFilter, int expand) throws Exception {
+        String strSQL1 = strSQL;
+        strSQL1 += SqlHelper.buildSqlFilter(alFilter);
+        strSQL1 += SqlHelper.buildSqlOrder(hmOrder);
+        strSQL1 += SqlHelper.buildSqlLimit(this.getCount(alFilter), intRegsPerPag, intPage);
+        ArrayList<ViewGenericBeanImplementation> aloBean = new ArrayList<>();
+        PreparedStatement oPreparedStatement = null;
+        ResultSet oResultSet = null;
+        MetaBeanHelper oMetaBeanHelper = null;
+        try {
+            oPreparedStatement = oConnection.prepareStatement(strSQL1);
+            oResultSet = oPreparedStatement.executeQuery(strSQL1);
+            while (oResultSet.next()) {
+                GenericBeanInterface oBean = BeanFactory.getBean(ob, oPuserSecurity);
+                oBean = (ViewGenericBeanImplementation) oBean.fill(oResultSet, oConnection, oPuserSecurity, expand);
+                aloBean.add((ViewGenericBeanImplementation) oBean);
+            }
+
+            ArrayList<MetaPropertyGenericBeanHelper> alMetaProperties = this.getPropertiesMetaData();
+            MetaObjectGenericBeanHelper oMetaObject = this.getObjectMetaData();
+            oMetaBeanHelper = new MetaBeanHelper(oMetaObject, alMetaProperties, aloBean);
+
+        } catch (Exception ex) {
+            String msg = this.getClass().getName() + ":" + (ex.getStackTrace()[0]).getMethodName() + " ob:" + ob;
+            Log4jHelper.errorLog(msg, ex);
+            throw new Exception(msg, ex);
+        } finally {
+            if (oResultSet != null) {
+                oResultSet.close();
+            }
+            if (oPreparedStatement != null) {
+                oPreparedStatement.close();
+            }
+        }
+        return oMetaBeanHelper;
+    }
+
+    public MetaBeanHelper getFromLoginAndPass(UsuarioSpecificBeanImplementation oUsuarioBean) throws Exception {
+        PreparedStatement oPreparedStatement = null;
+        ResultSet oResultSet = null;
+        MetaBeanHelper oMetaBeanHelper = null;
+        strSQL += " AND login='" + oUsuarioBean.getLogin() + "'";
+        strSQL += " AND password='" + oUsuarioBean.getPassword() + "'";
+        try {
+            oPreparedStatement = oConnection.prepareStatement(strSQL);
+            oResultSet = oPreparedStatement.executeQuery();
+            if (oResultSet.next()) {
+                oUsuarioBean.setId(oResultSet.getInt("id"));
+                oMetaBeanHelper = this.get(oUsuarioBean.getId(), 3);
+            } else {
+                throw new Exception("UsuarioDao getFromLoginAndPass error");
+            }
+        } catch (Exception ex) {
+            String msg = this.getClass().getName() + ":" + (ex.getStackTrace()[0]).getMethodName() + " ob:" + ob;
+            Log4jHelper.errorLog(msg, ex);
+            throw new Exception(msg, ex);
+        } finally {
+            if (oResultSet != null) {
+                oResultSet.close();
+            }
+            if (oPreparedStatement != null) {
+                oPreparedStatement.close();
+            }
+        }
+        return oMetaBeanHelper;
+    }
+
+    public Integer getIDfromUser(String strLogin) throws Exception {
+        Integer intResult = null;
+        Statement oStatement = null;
+        ResultSet oResultSet = null;
+        try {
+            oStatement = (Statement) oConnection.createStatement();
+            String strSQL = "SELECT id FROM usuario WHERE login ='" + strLogin + "'";
+            oResultSet = oStatement.executeQuery(strSQL);
+            if (oResultSet.next()) {
+                intResult = oResultSet.getInt("id");
+            } else {
+                return 0;
+            }
+        } catch (SQLException ex) {
+            String msg = this.getClass().getName() + ":" + (ex.getStackTrace()[0]).getMethodName() + " ob:" + ob;
+            Log4jHelper.errorLog(msg, ex);
+            throw new Exception(msg, ex);
+        } finally {
+            if (oResultSet != null) {
+                oResultSet.close();
+            }
+            if (oStatement != null) {
+                oStatement.close();
+            }
+        }
+        return intResult;
+    }
+
+    public Integer getIDfromCodigoGrupo(String strCode) throws Exception {
+        Integer intResult = null;
+        Statement oStatement = null;
+        ResultSet oResultSet = null;
+        try {
+            oStatement = (Statement) oConnection.createStatement();
+            String strSQL = "SELECT id FROM grupo WHERE codigo ='" + strCode + "'";
+            oResultSet = oStatement.executeQuery(strSQL);
+            if (oResultSet.next()) {
+                intResult = oResultSet.getInt("id");
+            } else {
+                return 0;
+            }
+        } catch (SQLException ex) {
+            String msg = this.getClass().getName() + ":" + (ex.getStackTrace()[0]).getMethodName() + " ob:" + ob;
+            Log4jHelper.errorLog(msg, ex);
+            throw new Exception(msg, ex);
+        } finally {
+            if (oResultSet != null) {
+                oResultSet.close();
+            }
+            if (oStatement != null) {
+                oStatement.close();
+            }
+        }
+        return intResult;
+    }
+}
--- src/main/java/eu/rafaelaznar/factory/DaoFactory.java
+++ src/main/java/eu/rafaelaznar/factory/DaoFactory.java
@@ -59,6 +59,7 @@ import eu.rafaelaznar.dao.specificimplementation.SexoSpecificDaoImplementation;
 import eu.rafaelaznar.dao.specificimplementation.TipodependenciaSpecificDaoImplementation;
 import eu.rafaelaznar.dao.specificimplementation.TiposervicioSpecificDaoImplementation;
 import eu.rafaelaznar.dao.specificimplementation.TipousuarioSpecificDaoImplementation;
+import eu.rafaelaznar.dao.specificimplementation.UsuarioAlumnoSpecificDaoImplementation;
 import eu.rafaelaznar.dao.specificimplementation.UsuarioSpecificDaoImplementation;
 import java.sql.Connection;
 
@@ -68,7 +69,30 @@ public class DaoFactory {
         MetaDaoInterface oDao = null;
         switch (ob) {
             case "usuario":
-                oDao = (MetaDaoInterface) new UsuarioSpecificDaoImplementation(oConnection, oPuserBean_security, strWhere);
+                if (oPuserBean_security == null) {
+                    oDao = (MetaDaoInterface) new UsuarioSpecificDaoImplementation(oConnection, oPuserBean_security, strWhere);
+                    break;
+                } else {
+                    oDao = (MetaDaoInterface) new UsuarioSpecificDaoImplementation(oConnection, oPuserBean_security, strWhere);
+                    UsuarioSpecificBeanImplementation oUsuario = (UsuarioSpecificBeanImplementation) oPuserBean_security.getBean();
+                    MetaBeanHelper oMetaBeanHelper = oUsuario.getObj_tipousuario();
+                    TipousuarioSpecificBeanImplementation oTipoUsuario = (TipousuarioSpecificBeanImplementation) oMetaBeanHelper.getBean();
+                    Integer idTipousuario = oTipoUsuario.getId();
+                    switch (idTipousuario) {
+                        case 1:
+                            oDao = (MetaDaoInterface) new UsuarioSpecificDaoImplementation(oConnection, oPuserBean_security, strWhere);
+                            break;
+                        case 3:
+                            oDao = (MetaDaoInterface) new UsuarioSpecificDaoImplementation(oConnection, oPuserBean_security, strWhere);
+                            break;
+                        case 4:
+                            oDao = (MetaDaoInterface) new UsuarioAlumnoSpecificDaoImplementation(oConnection, oPuserBean_security, strWhere);
+                            break;
+                        case 5:
+                            oDao = (MetaDaoInterface) new UsuarioSpecificDaoImplementation(oConnection, oPuserBean_security, strWhere);
+                            break;
+                    }
+                }
                 break;
             case "tipousuario":
                 oDao = (MetaDaoInterface) new TipousuarioSpecificDaoImplementation(oConnection, oPuserBean_security, strWhere);
@@ -120,6 +144,7 @@ public class DaoFactory {
                 break;
             case "paciente":
                 if (oPuserBean_security != null) { //en el proceso de login puede ser nulo!!
+                    oDao = (MetaDaoInterface) new UsuarioSpecificDaoImplementation(oConnection, oPuserBean_security, strWhere);
                     UsuarioSpecificBeanImplementation oUsuario = (UsuarioSpecificBeanImplementation) oPuserBean_security.getBean();
                     MetaBeanHelper oMetaBeanHelper = oUsuario.getObj_tipousuario();
                     TipousuarioSpecificBeanImplementation oTipoUsuario = (TipousuarioSpecificBeanImplementation) oMetaBeanHelper.getBean();
